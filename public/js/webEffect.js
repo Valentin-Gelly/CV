021ace4103c05db6e6a867ee78cf1dce763f8ee4
Vavaaaahhhh <gelly.valentin@gmail.com>
2023-02-28 19:01:16 +0100

    first commit

---
+++ public/js/webEffect.js
@@ -0,0 +1,30 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+document.querySelectorAll(".title").forEach((element) => {
+  element.onmouseover = e => {  
+    let interval = null;
+    
+    let intervalCount = 0
+
+    clearInterval(interval)
+
+    interval = setInterval(() => {
+      e.target.innerText = e.target.innerText.split("")
+      .map((letter, index) => {
+        if(index < intervalCount) {
+          return e.target.dataset.text[index];
+        }
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("")    
+
+      if(intervalCount >= e.target.dataset.text.length){
+        clearInterval(interval)      
+      }     
+      intervalCount += 1 / 2
+    }, 30)
+  }
+})
+
+/**smooth scroll */
+var easeInQuart = new SmoothScroll('[data-easing="easeInQuart"]', {easing: 'easeInQuart'});
